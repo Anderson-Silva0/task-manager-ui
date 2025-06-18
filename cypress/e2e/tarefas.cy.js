d1d008aaca7ff3ddf740cbdd6b27b3d0629bece1
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2025-06-17 22:37:00 -0300

    finalize E2E tests using Cypress for full user CRUD flow

---
+++ cypress/e2e/tarefas.cy.js
@@ -0,0 +1,68 @@
+describe("Testes de Tarefas", () => {
+  let nomeUsuario;
+  let emailUsuario;
+
+  beforeEach(() => {
+    cy.visit("/users");
+    const timestamp = Date.now();
+    nomeUsuario = `Usuário Teste ${timestamp}`;
+    emailUsuario = `usuario${timestamp}@teste.com`;
+
+    cy.get("button.btn-success").should("be.visible").click();
+    cy.get('input[formControlName="name"]').type(nomeUsuario);
+    cy.get('input[formControlName="email"]').type(emailUsuario);
+    cy.get('button[type="submit"]').click();
+    cy.get("table").should("contain", nomeUsuario);
+
+    cy.visit("/tasks");
+
+    cy.get(".fa-spinner").should("not.exist");
+  });
+
+  it("deve cadastrar uma nova tarefa", () => {
+    const timestamp = Date.now();
+    const titulo = `Tarefa Teste ${timestamp}`;
+    const descricao = `Descrição da tarefa ${timestamp}`;
+
+    cy.get("button.btn-success").should("be.visible").click();
+    cy.get('input[formControlName="title"]').type(titulo);
+    cy.get('textarea[formControlName="description"]').type(descricao);
+    cy.get('select[formControlName="userId"]').select(
+      `${nomeUsuario} (${emailUsuario})`
+    );
+    cy.get('button[type="submit"]').click();
+    cy.get(".card-title").should("contain", titulo);
+    cy.get(".card-text").should("contain", descricao);
+  });
+
+  it("deve listar as tarefas cadastradas", () => {
+    cy.get(".card").should("be.visible");
+    cy.get(".card").should("have.length.greaterThan", 0);
+  });
+
+  it("Update - deve editar uma tarefa existente e marcar como concluída", () => {
+    const timestamp = Date.now();
+    const novoTitulo = `Título Editado ${timestamp}`;
+
+    cy.get("button.btn-outline-primary").first().click();
+    cy.get('input[formControlName="title"]').clear().type(novoTitulo);
+    cy.get('select[formControlName="status"]').select("CONCLUIDO");
+    cy.get('button[type="submit"]').click();
+    cy.get(".card-title").should("contain", novoTitulo);
+    cy.get(".badge.bg-success").should("contain", "Concluído");
+  });
+
+  it("não deve permitir edição de tarefa com status concluído", () => {
+    cy.get("button.btn-outline-primary").first().click();
+    cy.contains("Não é possível editar tarefas concluídas").should(
+      "be.visible"
+    );
+  });
+
+  it("Delete - deve excluir uma tarefa", () => {
+    cy.get("button.btn-danger").first().click();
+    cy.get("mat-dialog-container").should("be.visible");
+    cy.get('mat-dialog-actions button[color="warn"]').click();
+    cy.contains("Tarefa excluída com sucesso").should("be.visible");
+  });
+});
--- cypress/e2e/usuarios.cy.js
+++ cypress/e2e/usuarios.cy.js
@@ -1,40 +1,97 @@
-describe('CRUD de Usuários', () => {
+describe("CRUD de Usuários", () => {
+  let emailCriado;
+  let nomeUsuario;
+
   beforeEach(() => {
-    cy.visit('/users');
+    cy.visit("/users");
   });
 
-  it('deve cadastrar um novo usuário', () => {
+  it("deve cadastrar um novo usuário", () => {
     const timestamp = Date.now();
-    const nome = `Usuário Teste ${timestamp}`;
-    const email = `usuario${timestamp}@teste.com`;
+    nomeUsuario = `Usuário Teste ${timestamp}`;
+    emailCriado = `usuario${timestamp}@teste.com`;
 
-    cy.get('button.btn-success').should('be.visible').click();
-    cy.get('input[formControlName="name"]').type(nome);
-    cy.get('input[formControlName="email"]').type(email);
+    cy.get("button.btn-success").should("be.visible").click();
+    cy.get('input[formControlName="name"]').type(nomeUsuario);
+    cy.get('input[formControlName="email"]').type(emailCriado);
     cy.get('button[type="submit"]').click();
-    cy.get('table').should('contain', nome);
-    cy.get('table').should('contain', email);
+    cy.get("table").should("contain", nomeUsuario);
+    cy.get("table").should("contain", emailCriado);
   });
 
-  it('deve listar os usuários cadastrados', () => {
-    cy.get('table').should('be.visible');
-    cy.get('table').find('tr').should('have.length.greaterThan', 1);
+  it("deve listar os usuários cadastrados", () => {
+    cy.get("table").should("be.visible");
+    cy.get("table").find("tr").should("have.length.greaterThan", 1);
   });
 
-  it('Update - deve editar um usuário existente', () => {
+  it("Update - deve editar um usuário existente", () => {
     const timestamp = Date.now();
     const novoNome = `Nome Editado ${timestamp}`;
-    
-    cy.get('button.btn-outline-primary').first().click();
+
+    cy.get("button.btn-outline-primary").first().click();
     cy.get('input[formControlName="name"]').clear().type(novoNome);
     cy.get('button[type="submit"]').click();
-    cy.get('table').should('contain', novoNome);
+    cy.get("table").should("contain", novoNome);
   });
 
-  it('Delete - deve excluir um usuário sem tarefas', () => {
-    cy.get('button.btn-danger').last().click();
-    cy.get('mat-dialog-container').should('be.visible');
-    cy.get('mat-dialog-actions button[color="warn"]').click();
-    cy.contains('Usuário excluído com sucesso').should('be.visible');
+  it("não deve permitir cadastro de usuário com email duplicado", () => {
+    cy.get("button.btn-success").should("be.visible").click();
+    cy.get('input[formControlName="name"]').type("Usuário Duplicado");
+    cy.get('input[formControlName="email"]').type(emailCriado);
+    cy.get('button[type="submit"]').click();
+    cy.contains("Email já cadastrado").should("be.visible");
   });
-}); 
+
+  it("Delete - deve excluir um usuário sem tarefas", () => {
+    cy.get("button.btn-danger").last().click();
+    cy.get("mat-dialog-container").should("be.visible");
+    cy.get('mat-dialog-actions button[color="warn"]').click();
+    cy.contains("Usuário excluído com sucesso").should("be.visible");
+  });
+
+  it("não deve permitir exclusão de usuário com tarefas cadastradas", () => {
+    const timestamp = Date.now();
+    const nomeUsuarioComTarefa = `Usuário Com Tarefa ${timestamp}`;
+    const emailUsuarioComTarefa = `comtarefa${timestamp}@teste.com`;
+
+    cy.visit("/users");
+    cy.get("button.btn-success").should("be.visible").click();
+    cy.get('input[formControlName="name"]').type(nomeUsuarioComTarefa);
+    cy.get('input[formControlName="email"]').type(emailUsuarioComTarefa);
+    cy.get('button[type="submit"]').click();
+    cy.get("table").should("contain", nomeUsuarioComTarefa);
+
+    cy.visit("/tasks");
+    const titulo = `Tarefa Teste ${timestamp}`;
+    const descricao = `Descrição da tarefa ${timestamp}`;
+
+    cy.get("button.btn-success").should("be.visible");
+
+    cy.get("button.btn-success").click();
+
+    cy.get('select[formControlName="userId"]').should("be.visible");
+    cy.get('select[formControlName="userId"] option').should(
+      "have.length.greaterThan", 1);
+
+    cy.get('input[formControlName="title"]').type(titulo);
+    cy.get('textarea[formControlName="description"]').type(descricao);
+
+    cy.get('select[formControlName="userId"]').select(
+      `${nomeUsuarioComTarefa} (${emailUsuarioComTarefa})`
+    );
+
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".card-title").should("contain", titulo);
+
+    cy.visit("/users");
+    cy.contains(nomeUsuarioComTarefa)
+      .parent()
+      .find("button.btn-danger")
+      .click();
+    cy.get("mat-dialog-container").should("be.visible");
+    cy.get('mat-dialog-actions button[color="warn"]').click();
+
+    cy.contains("Usuário possui tarefas associadas e não pode ser deletado.").should("be.visible");
+  });
+});
